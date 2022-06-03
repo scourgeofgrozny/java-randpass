6515f1c962bf7431c9d49d85199223c28ca3c18e
MichelinMan <98357487+MichelinMan69@users.noreply.github.com>
2022-06-03 02:32:38 -0400

    Update randpass.java

--- randpass.java
+++ randpass.java
@@ -2,17 +2,13 @@ package src.javarandpass;
 import java.util.Random;
 import java.util.TimerTask;
 import java.util.Timer;
-
 public class randpass {
-    
     public static void main(String[] args){
-        
     int max = 9;
     int min = 0;
     Random num = new Random();
     Random letter = new Random();
-    Timer myTimer = new Timer();
-        
+    Timer myTimer = new Timer(); 
     for (int i = 0; i < 2; i++){
         myTimer.schedule(new TimerTask(){
             @Override
@@ -21,7 +17,6 @@ public class randpass {
                 char ll = (char)(letter.nextInt(26) + 'a');
                 char lu = (char)(letter.nextInt(26) + 'A');
                 int n = min + num.nextInt(max);
-
                 System.out.print(ll);
                 System.out.print(n);
                 System.out.print(lu);
